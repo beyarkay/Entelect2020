892cde02e390ae5580b0b4e7be5d1a0809faa0de
beyarkay <boydrkane@gmail.com>
2020-07-31 17:57:01 +0200

    Added file writer method

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,66 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("Hello world");
-	}
 
-	String[] inFiles = new String[]{
-			"inputs/map_1.input",
-			"inputs/map_2.input",
-			"inputs/map_3.input",
-			"inputs/map_4.input",
-			"inputs/map_5.input"
-	};
-	String[] outFiles = new String[]{
+	static String[] outFiles = new String[]{
 			"outputs/map_1.txt",
 			"outputs/map_2.txt",
 			"outputs/map_3.txt",
 			"outputs/map_4.txt",
 			"outputs/map_5.txt"
 	};
+	private static String[] inFiles = new String[]{
+			"inputs/map_1.input",
+			"inputs/map_2.input",
+			"inputs/map_3.input",
+			"inputs/map_4.input",
+			"inputs/map_5.input"
+	};
 
-	public static ArrayList<String> readInput(String fileName) throws IOException {
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+
+		for (int i = 0; i < inFiles.length; i++) {
+			ArrayList<String> lines = readInput(inFiles[i]);
+			// Process the file with an algorithm, machine learning, artificial intelligence and Superior Intellect
+			writeFile(outFiles[i], lines);
+		}
+	}
+
+	private static void writeFile(String inFile, ArrayList<String> lines) {
+		try {
+			FileWriter writer = new FileWriter(inFile);
+			for (int i = 0; i < lines.size(); i++) {
+				writer.write(lines.get(i));
+			}
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+
+	private static ArrayList<String> readInput(String fileName) {
 		ArrayList<String> lines = new ArrayList<>();
-		BufferedReader br = new BufferedReader(new FileReader(fileName));
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader(fileName));
+
 //		String[] firstLineParts = br.readLine().split(",");
 
-		String line;
-		while ((line = br.readLine()) != null) {
-			lines.add(line);
+			String line;
+			while ((line = br.readLine()) != null) {
+				lines.add(line);
+			}
+			br.close();
+			return lines;
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		br.close();
-		return lines;
+		return null;
 	}
 }
