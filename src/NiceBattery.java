31020505d790c451bc6fd1d95da462468ae01d28
Stuart Mesham <stumes3@gmail.com>
2020-08-01 11:28:17 +0200

    added Nice Battery

---
+++ src/NiceBattery.java
@@ -0,0 +1,20 @@
+public class NiceBattery {
+	public Shape shape;
+	public int rotation;
+	public int row, col;
+
+	public NiceBattery(Shape shape, int rotation, int row, int col) {
+		this.shape = shape;
+		this.rotation = rotation;
+		this.row = row;
+		this.col = col;
+	}
+
+	public Battery toShitBattery() {
+		Battery shitBattery = new Battery(shape.id, shape.boundingBox, shape.capacity, shape.mass, shape.shapeData);
+		shitBattery.offset[0] = row;
+		shitBattery.offset[1] = col;
+		shitBattery.rotationId = rotation;
+		return shitBattery;
+	}
+}
