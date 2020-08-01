5f52111167c5174f270e4182f37352bfcec92044
beyarkay <boydrkane@gmail.com>
2020-08-01 14:54:24 +0200

    slight slider optimisation

--- src/Boyd.java
+++ src/Boyd.java
@@ -7,12 +7,12 @@ public class Boyd {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 
-		for (int i = 4; i < Utils.inFiles.length; i++) {
+		for (int i = 3; i < Utils.inFiles.length; i++) {
 			System.out.println("Calculating: " + Utils.inFiles[i]);
 			try {
 				Utils.ProblemSpecification problemSpec = Utils.readInput("inputs/shapes_file.json", Utils.inFiles[i]);
 
-				Battery[] batteries = slider(problemSpec);
+				Battery[] batteries = slider(problemSpec, Utils.outFiles[i]);
 				Utils.writeFile(Utils.outFiles[i], batteries);
 //				break;
 
@@ -98,7 +98,7 @@ public class Boyd {
 	}
 
 
-	public static Battery[] slider(Utils.ProblemSpecification ps) {
+	public static Battery[] slider(Utils.ProblemSpecification ps, String filename) {
 
 		// Read in PS and convert it to shapes
 		Shape[] shapes = ps.shapes;
@@ -128,6 +128,8 @@ public class Boyd {
 		ArrayList<Battery> batteries = new ArrayList<>();
 		// for each shape
 		int lastId = -1;
+		int[] lastOffset = new int[]{0, 0};
+		long start = System.currentTimeMillis();
 		long t = System.currentTimeMillis();
 		long ttotal = 1;
 		float ignoreme = 0.2f;
@@ -139,43 +141,55 @@ public class Boyd {
 				System.out.println("Checked " + i + "/" + totalShapes + "(" + Math.round(((float) i) / totalShapes * 100) + ")");
 				ttotal += System.currentTimeMillis()-t;
 				System.out.println("Taken " + ttotal/1000 + "s");
-				System.out.println("ETA "+(float)(ttotal)/(float)((float)(i+1)/(float)totalShapes));
+				System.out.println("ETA " + (float) (ttotal) / ((float) (i + 1) / (float) totalShapes));
 			}//			System.out.println("id:" + currShape.id);
 
-			if (lastId == currShape.id) {
-//				System.out.println("Skipping");
-				continue;
+			if (i % 100 == 0) {
+				int shapesComplete = totalShapes - i;
+				float millisPerShape = (System.currentTimeMillis() - start) / ((float) i);
+				float doneIn = shapesComplete * millisPerShape;
+				double doneInMinutes = doneIn / 60.0;
+				System.out.println("Done " + i + "/" + totalShapes + " (" + Math.round(((float) i) / totalShapes * 100) + "%) done in: " + doneInMinutes);
 			}
-
+			if (i % 10000 == 0) {
+				Utils.writeFile(filename, batteries.toArray(new Battery[0]));
+			}
+//			if (lastId == currShape.id) {
+////				System.out.println("Skipping");
+//				continue;
+//			}
 			//for each offset row
-			for (int r = 0; r < ps.rows; r++) {
+			for (int r = lastOffset[0]; r < ps.rows + lastOffset[0]; r++) {
+				int rmod = r % ps.rows;
 				// for each offset col
-				for (int c = 0; c < ps.columns; c++) {
+				for (int c = lastOffset[1]; c < ps.columns + lastOffset[1]; c++) {
+					int cmod = c % ps.columns;
 					// for each rotation
 					for (int rot = 0; rot < 4; rot++) {
 						// check if the current shape can fit
 						boolean collision = false;
 						for (int cellIndex = 0; cellIndex < currShape.shapeData[rot].length; cellIndex++) {
-							int currRow = currShape.shapeData[rot][cellIndex][0] + r;
-							int currCol = currShape.shapeData[rot][cellIndex][1] + c;
+							int currRow = currShape.shapeData[rot][cellIndex][0] + rmod;
+							int currCol = currShape.shapeData[rot][cellIndex][1] + cmod;
 							if (currRow < 0 || currRow >= ps.rows || currCol < 0 || currCol >= ps.columns || grid.grid[currRow][currCol] != 0) {
 								collision = true;
 								break;
 							}
 						}
-						if (!collision) {
-							// Add the battery
+						if (!collision) {       // there is no collision and we can place the battery
 							for (int cellIndex = 0; cellIndex < currShape.shapeData[rot].length; cellIndex++) {
-								int currRow = currShape.shapeData[rot][cellIndex][0] + r;
-								int currCol = currShape.shapeData[rot][cellIndex][1] + c;
+								int currRow = currShape.shapeData[rot][cellIndex][0] + rmod;
+								int currCol = currShape.shapeData[rot][cellIndex][1] + cmod;
 								grid.grid[currRow][currCol] = bshapes[i].id;
 							}
 							Battery currBattery = new Battery(bshapes[i]);
 							currBattery.rotationId = rot;
-							currBattery.offset[0] = r;
-							currBattery.offset[1] = c;
+							currBattery.offset[0] = rmod;
+							currBattery.offset[1] = cmod;
 							batteries.add(currBattery);
 							placed = true;
+							lastOffset[0] = rmod;
+							lastOffset[1] = cmod;
 							break;
 						}
 					}
@@ -184,7 +198,6 @@ public class Boyd {
 					}
 				}
 				if (placed) {
-//					System.out.println(grid.toASCII());
 					break;
 				}
 			}
